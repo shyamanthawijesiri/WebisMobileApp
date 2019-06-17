2b043e02b82b352225df6ac7d70f80d53f84645b
shyamanthawijesiri <shyamanthawijesiri33@gmail.com>
2019-06-17 19:21:28 +0530

    signin and login modal

--- src/app/home/signin/signin.module.ts
+++ src/app/home/signin/signin.module.ts
@@ -6,6 +6,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { SigninPage } from './signin.page';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
@@ -21,6 +23,7 @@ const routes: Routes = [
     IonicModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [SigninPage]
+  declarations: [SigninPage, SignupComponent,LoginComponent],
+  entryComponents:[SignupComponent, LoginComponent]
 })
 export class SigninPageModule {}
--- src/app/home/signin/signin.page.ts
+++ src/app/home/signin/signin.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SignupComponent } from './signup/signup.component';
+import { ModalController } from '@ionic/angular';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninPage implements OnInit {
 
-  constructor() { }
+  constructor(private modalCtrl: ModalController) { }
 
   ngOnInit() {
   }
 
+  async onSignUp() {
+    const modal = await this.modalCtrl.create({
+      component: SignupComponent ,
+      componentProps: { value: 123 }
+    });
+    return await modal.present();
+  }
+
+  async onLogin(){
+    const modal = await this.modalCtrl.create({
+      component: LoginComponent ,
+      componentProps: { value: 123 }
+    });
+    return await modal.present();
+
+  }
 }
